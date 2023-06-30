8e3e6a8e6458f5de4ba62471f585c2e18d4287a9
Ottar Hellan <hellanottar@simula.no>
2023-06-30 16:55:35 +0000

    Faster reloading of femnets into fenics, assuming the same function space is used

--- tools/loading.py
+++ tools/loading.py
@@ -63,12 +63,24 @@ def fenics_to_femnet(u: df.Function) -> Femnet:
 
     return u_fn
 
+
+    # def set_from_coefficients(self, coefs):
+    #     '''Set the degrees of freedom'''
+    #     with torch.no_grad():
+    #         self.lin.weight[0] = torch.tensor(coefs)
+
 def femnet_to_fenics(u_fn: Femnet, V: df.FunctionSpace) -> df.Function:
     """
         Is inefficient for now, as it evaluates the network at all dof points, meaning it needs
         to build a large vandermonde matrix, instead of using coefficients which might be
         stored good enough in the network.
     """
+
+    # u = df.Function(V)
+    # u.vector().set_local(u_fn.lin.weight[0].detach().numpy())
+
+    # return u
+
     if V.num_sub_spaces() > 0:
         return femnet_to_fenics_vector(u_fn, V)
     else:
@@ -77,31 +89,44 @@ def femnet_to_fenics(u_fn: Femnet, V: df.FunctionSpace) -> df.Function:
 def femnet_to_fenics_vector(u_fn: Femnet, V: df.FunctionSpace) -> df.Function:
 
     u = df.Function(V)
-    dof_coordinates = V.tabulate_dof_coordinates()[::2]
-    x_torch = torch.tensor(dof_coordinates[None,...], dtype=torch.double)
-    y_torch = u_fn(x_torch)
-    y_np = y_torch[0,...].detach().numpy()
 
-    new_u_dofs = np.zeros(dof_coordinates.shape[0]*2, dtype=float)
+    new_u_dofs = np.zeros_like(u.vector()[:])
+    new_u_dofs[::2] = u_fn.lins[0].weight[0].detach().numpy()
+    new_u_dofs[1::2] = u_fn.lins[1].weight[0].detach().numpy()
 
-    new_u_dofs[::2] = y_np[:,0]  # Insert the x_dofs from femnet
-    new_u_dofs[1::2] = y_np[:,1] # Insert the y_dofs from femnet
+    u.vector().set_local(new_u_dofs) # Seems to work on what it's tested on
 
-    u2 = df.Function(V)
-    u2.vector()[:] = new_u_dofs
+    # dof_coordinates = V.tabulate_dof_coordinates()[::2]
+    # x_torch = torch.tensor(dof_coordinates[None,...], dtype=torch.double)
+    # y_torch = u_fn(x_torch)
+    # y_np = y_torch[0,...].detach().numpy()
 
-    return u2
+    # new_u_dofs = np.zeros(dof_coordinates.shape[0]*2, dtype=float)
+
+    # new_u_dofs[::2] = y_np[:,0]  # Insert the x_dofs from femnet
+    # new_u_dofs[1::2] = y_np[:,1] # Insert the y_dofs from femnet
+
+    # u2 = df.Function(V)
+    # u2.vector()[:] = new_u_dofs
+
+    # return u2
+    
+    return u
+
 
 def femnet_to_fenics_scalar(u_fn: Femnet, V: df.FunctionSpace) -> df.Function:
 
     u = df.Function(V)
-    dof_coordinates = V.tabulate_dof_coordinates()
+    u.vector().set_local(u_fn.lin.weight[0].detach().numpy()) # Seems to work
 
-    x_torch = torch.tensor(dof_coordinates[None,...], dtype=torch.double)
-    y_torch = u_fn(x_torch)
-    y_np = y_torch[0,...].detach().numpy()
+    # dof_coordinates = V.tabulate_dof_coordinates()
 
-    u2 = df.Function(V)
-    u2.vector()[:] = y_np
+    # x_torch = torch.tensor(dof_coordinates[None,...], dtype=torch.double)
+    # y_torch = u_fn(x_torch)
+    # y_np = y_torch[0,...].detach().numpy()
 
-    return u2
+    # u2 = df.Function(V)
+    # u2.vector()[:] = y_np
+    # return u2
+
+    return u
