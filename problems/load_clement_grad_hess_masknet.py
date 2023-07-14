Fixed training not using the masking function.

problems/load_clement_grad_hess_masknet.py
@@ -69,7 +69,7 @@ cost_function = nn.MSELoss()
 optimizer = torch.optim.LBFGS(mlp.parameters(), line_search_fn="strong_wolfe") # Good batch size: 16
 
 
-context = Context(network, cost_function, optimizer)
+context = Context(mask_net, cost_function, optimizer)
 context.load("models/ADAM_16_128_2_clm_grad_hess")
 # context.load("models/mask_ex_LBFGS_8_128_2_clm")
 

problems/load_clement_grad_masknet.py
@@ -69,9 +69,11 @@ cost_function = nn.MSELoss()
 optimizer = torch.optim.LBFGS(mlp.parameters(), line_search_fn="strong_wolfe") # Good batch size: 16
 
 
-context = Context(network, cost_function, optimizer)
-context.load("models/LBFGS_8_128_2_clm")
+context = Context(mask_net, cost_function, optimizer)
+# context.load("models/LBFGS_8_128_2_clm")
+# context.load("models/new_mask_LBFGS_8_128_2_clm_grad")
 # context.load("models/mask_ex_LBFGS_8_128_2_clm")
+context.load("models/MAE_LBFGS_8_128_2_clm_grad")
 
 
 # print(x.shape)
@@ -96,14 +98,23 @@ u_diff.vector().set_local(new_dofs)
 # outfile << u_diff
 
 
-print(torch.mean(torch.abs(y - x[...,:2])))
-print(torch.mean(torch.abs(mask_net.network(x))))
-print(torch.mean(torch.abs(mask_net.network(x)*mask_net.mask(x)[...,None])))
-print(torch.mean(torch.abs(y - mask_net(x))))
 
-print(torch.min(mask_net.mask.x))
-print(torch.max(mask_net.mask.x))
+print(torch.mean(torch.linalg.norm(y-x[...,:2], ord=1, dim=-1)))
+print(torch.mean(torch.linalg.norm(mask_net.network(x), ord=1, dim=-1)))
+print(torch.mean(torch.linalg.norm(mask_net.network(x)*mask_net.mask(x)[...,None], ord=1, dim=-1)))
+print(torch.mean(torch.linalg.norm(y - mask_net(x), ord=1, dim=-1)))
+#MAE
+print()
 
+print(torch.sqrt(torch.mean(torch.linalg.norm(y - x[...,:2], ord=2, dim=-1)**2)))
+print(torch.sqrt(torch.mean(torch.linalg.norm(mask_net.network(x), ord=2, dim=-1)**2)))
+print(torch.sqrt(torch.mean(torch.linalg.norm(mask_net.network(x)*mask_net.mask(x)[...,None], ord=2, dim=-1)**2)))
+print(torch.sqrt(torch.mean(torch.linalg.norm(y - mask_net(x), ord=2, dim=-1)**2)))
+#RMSE
+
+
+# print(torch.linalg.norm(y - x[...,:2], dim=-1).shape)
+# print(y.shape)
 
 # A_1 = mask_net.network[1].layers[0].weight.detach().clone()
 # print(A_1)

problems/run_clement_grad_hess_masknet.py
@@ -125,7 +125,7 @@ def main():
     optimizer = torch.optim.LBFGS(mlp.parameters(), line_search_fn="strong_wolfe") # Good batch size: 16?
 
 
-    context = Context(network, cost_function, optimizer)
+    context = Context(mask_net, cost_function, optimizer)
 
     print(context)
 

problems/run_clement_grad_masknet.py
@@ -59,10 +59,10 @@ def main():
 
     V_scal = df.FunctionSpace(fluid_mesh, "CG", 1) # Linear scalar polynomials over triangular mesh
 
-    mask_df = poisson_mask(V_scal, normalize = True)
-    # from conf import poisson_mask_f
-    # from networks.masknet import poisson_mask_custom
-    # mask_df = poisson_mask_custom(V_scal, poisson_mask_f, normalize = True)
+    # mask_df = poisson_mask(V_scal, normalize = True)
+    from conf import poisson_mask_f
+    from networks.masknet import poisson_mask_custom
+    mask_df = poisson_mask_custom(V_scal, poisson_mask_f, normalize = True)
 
     mask_tensor = torch.tensor(mask_df.vector().get_local(), dtype=torch.get_default_dtype())
     mask = TensorModule(mask_tensor)
@@ -73,6 +73,7 @@ def main():
     # base returns (u_x, u_y) from (u_x, u_y, d_x u_x, d_y u_x, d_x u_y, d_y u_y)
 
     widths = [8, 128, 2]
+    # widths = [8, 32, 2]
     mlp = MLP(widths, activation=nn.ReLU())
     # MLP takes input (x, y, u_x, u_y, d_x u_x, d_y u_x, d_x u_y, d_y u_y)
 
@@ -112,13 +113,14 @@ def main():
     print("Pre-run assertions passed. \n")
 
 
-    cost_function = nn.MSELoss()
+    # cost_function = nn.MSELoss()
+    cost_function = nn.L1Loss()
     # optimizer = torch.optim.SGD(mlp.parameters(), lr=1e-1)
-    optimizer = torch.optim.Adam(mlp.parameters()) # Good batch size: 1024?
-    # optimizer = torch.optim.LBFGS(mlp.parameters(), line_search_fn="strong_wolfe") # Good batch size: 16
+    # optimizer = torch.optim.Adam(mlp.parameters()) # Good batch size: 1024?
+    optimizer = torch.optim.LBFGS(mlp.parameters(), line_search_fn="strong_wolfe") # Good batch size: 16
 
 
-    context = Context(network, cost_function, optimizer)
+    context = Context(mask_net, cost_function, optimizer)
 
     print(context)
 
@@ -128,7 +130,7 @@ def main():
     # callback = None
 
     # num_epochs = 10
-    num_epochs = 30
+    num_epochs = 10
 
     start = timer()
 
@@ -140,13 +142,13 @@ def main():
     # print(f"{widths=}")
     print(f"T = {(end - start):.2f} s")
 
-    # context.save("models/mask_ex_LBFGS_8_128_2_clm")
+    context.save("models/MAE_LBFGS_8_128_2_clm_grad")
 
 
-    # plt.figure()
-    # # plt.plot(range(context.epoch), context.train_hist, 'k-')
-    # plt.semilogy(range(context.epoch), context.train_hist, 'k-')
-    # plt.savefig("foo/clm/mask_ex_LBFGS_train_hist.png", dpi=150)
+    plt.figure()
+    # plt.plot(range(context.epoch), context.train_hist, 'k-')
+    plt.semilogy(range(context.epoch), context.train_hist, 'k-')
+    plt.savefig("foo/clm/MAE_LBFGS_8_128_2_clm_grad_train_hist.png", dpi=150)
 
 
     return