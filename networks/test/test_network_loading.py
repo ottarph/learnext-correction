575d319c82b932fb82bbc324f34d02c2b40880b8
Ottar Hellan <hellanottar@simula.no>
2023-08-07 15:21:25 +0200

    Loading networks from .yml or .json with testing. Note: not good for static analysis.

---
+++ networks/test/test_network_loading.py
@@ -0,0 +1,37 @@
+from networks.loading import *
+
+def test_load_yml():
+
+    model_dir = "networks/test/test_model"
+    model = ModelLoader(model_dir, load_state_dict=False, mode="yaml")
+
+    assert model[1].layers[0].in_features == 2
+    assert torch.equal(model[0].x_mean, torch.tensor([0.321, 0.42]))
+
+    return
+
+def test_load_json():
+
+    model_dir = "networks/test/test_model"
+    model = ModelLoader(model_dir, load_state_dict=False, mode="json")
+
+    assert model[1].layers[0].in_features == 2
+    assert torch.equal(model[0].x_mean, torch.tensor([0.321, 0.42]))
+
+    return
+
+def test_load_state_dict():
+    model_dir = "networks/test/test_model"
+    model: nn.Module = ModelLoader(model_dir, load_state_dict=True, mode="yaml")
+
+    # model[1].layers[0].weight.data = torch.zeros_like(model[1].layers[0].weight) + 2.0
+    # torch.save(model.state_dict(), f"{model_dir}/state_dict.pt")
+
+    assert torch.equal(model[1].layers[0].weight, torch.zeros_like(model[1].layers[0].weight) + 2.0)
+
+    return
+
+if __name__ == "__main__":
+    test_load_yml()
+    test_load_json()
+    test_load_state_dict()
