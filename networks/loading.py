ccf876c8a55e1693e125e28a30068ed17dd1bec9
Ottar Hellan <hellanottar@simula.no>
2023-08-07 21:23:44 +0200

    Changed ModelLoader.__new__ to only call load_model.

--- networks/loading.py
+++ networks/loading.py
@@ -48,24 +48,8 @@ class ModelLoader:
 
     def __new__(cls, model_dir: PathLike, load_state_dict: bool = True,
                 mode: Literal["yaml", "json"] = "yaml") -> nn.Module:
-        model_dir = Path(model_dir)
-        if not model_dir.is_dir():
-            raise ValueError("Non-existent directory.")
-
-        if mode == "yaml":
-            with open(model_dir / "model.yml", "r") as infile:
-                model_dict = yaml.safe_load(infile.read())
-        elif mode == "json":
-            with open(model_dir / "model.json", "r") as infile:
-                model_dict= json.loads(infile.read())
-        else:
-            raise ValueError
-        model: nn.Module = build_model(model_dict)
-
-        if load_state_dict:
-            model.load_state_dict(torch.load(model_dir / "state_dict.pt"))
-
-        return model
+        
+        return load_model(model_dir, load_state_dict=load_state_dict, mode=mode)
 
 
 def build_model(model_dict: dict) -> nn.Module:
