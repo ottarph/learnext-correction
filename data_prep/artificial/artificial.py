248d53a191156cea68baa9e6181e1107e89c3503
Ottar Hellan <hellanottar@simula.no>
2023-08-09 15:14:56 +0200

    Sketch of classes to make artificial dataset.

---
+++ data_prep/artificial/artificial.py
@@ -0,0 +1,37 @@
+
+
+class DeformationFactory:
+    """
+        Creates instances of deformation of the solid domain
+    """
+    pass
+
+class BoundaryForceGenerator:
+    """
+        Generates random instances of boundary forces on
+        the solid to be turned into deformed solids by
+        ElasticSolver.
+    """
+    pass
+
+class ElasticSolver:
+    """
+        Takes boundary forces on the solid domain and turns
+        them into elastically deformed solid domain.
+
+        Are deformations made by linear elasticity and 
+        nonlinear elasticitiy too different for our case?
+    """
+    pass
+
+class BiharmonicExtender:
+    """
+        Takes deformations made by ``DeformationFactory`` and
+        turns them into biharmonic extensions over the 
+        fluid domain.
+    """
+    pass
+
+
+
+
